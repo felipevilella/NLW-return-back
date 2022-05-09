afdf6dfd0fe0b9aa188740a1afff8dfdb4cc5ad3
Felipe Alves <felipe.alves@gcbinvestivementos.com>
2022-05-08 21:04:53 -0300

    feat: implements screanshot

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -39,6 +39,8 @@ export class SubmitFeedbackUseCases {
                 `<div style= "font-family: sans-serif; font-size: 16px; color: #111;">`,
                 `<p> Tipo do feedback: ${type} </p>`,
                 `<p> Comentário: ${comment} </p>`,
+                screenshot ? `<img src="${screenshot} />`: null,
+                `</div>`
             ].join('\n')
         })
     }
